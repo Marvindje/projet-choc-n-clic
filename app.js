253ac564bc436b91fc4718827459fa8a64b8481b
Killian Cousseau <killian.cousseau@gmail.com>
2023-05-18 14:36:05 +0200

    Ajout du prompt non finalisé

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+//Nom de la boulangerie
 const bakery = document.querySelector(".bakery");
 
+//Fenêtre prompt
+const promptWindow = document.querySelector(".prompt");
+promptWindow.classList.add("invisible");
+
+//Input du nom de la boulangerie
+const bakeryName = document.querySelector("#bakeryName");
+
+//Cliquer sur le nom de la boulangerie affiche un prompt pour le changer
 bakery.addEventListener("click", function () {
-  name = prompt("What's the name of your bakery ?");
-  bakery.innerHTML = name;
+  promptWindow.classList.remove("invisible");
+});
+
+//Appuyer sur Entrée change le nom de la boulangerie et ferme le prompt
+bakeryName.addEventListener("keyup", function (event) {
+  if (event.key === "Enter" && bakeryName.value !== "") {
+    bakery.innerHTML = bakeryName.value;
+    promptWindow.classList.add("invisible");
+  }
+});
+
+//Bouton confirmer du prompt
+const confirmButton = document.querySelector(".confirm");
+
+//Appuyer sur le bouton confirme le changement de nom et ferme le prompt
+confirmButton.addEventListener("click", function () {
+  if (bakeryName.value !== "") {
+    bakery.innerHTML = bakeryName.value;
+    promptWindow.classList.add("invisible");
+  }
+});
+
+//Bouton annuler du prompt
+const cancelButton = document.querySelector(".cancel");
+
+//Appuyer sur le bouton ferme le prompt
+cancelButton.addEventListener("click", function () {
+  promptWindow.classList.add("invisible");
 });
