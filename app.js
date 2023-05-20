6c6f21369467e74cca28ffb45f834f2a362bda8b
Killian Cousseau <killian.cousseau@gmail.com>
2023-05-20 22:06:02 +0200

    Première intégration partie killian

--- app.js
+++ app.js
@@ -3,7 +3,65 @@ function togglePopup() {
   let popup = document.querySelector("#popup-overlay");
   popup.classList.toggle("open");
 }
-//------------------------------------------------------
+//--------------------------------- killian --------------------------------//
+
+//Nom de la boulangerie
+const bakery = document.querySelector(".bakery");
+
+//Fenêtre prompt
+const promptWindow = document.querySelector(".prompt");
+promptWindow.classList.add("invisible");
+
+//Input du nom de la boulangerie
+const bakeryName = document.querySelector("#bakeryName");
+
+//Cliquer sur le nom de la boulangerie affiche un prompt pour le changer
+bakery.addEventListener("click", function () {
+  promptWindow.classList.remove("invisible");
+  background.classList.remove("invisible");
+});
+
+//Appuyer sur Entrée change le nom de la boulangerie et ferme le prompt
+bakeryName.addEventListener("keyup", function (event) {
+  if (event.key === "Enter" && bakeryName.value !== "") {
+    bakery.innerHTML = bakeryName.value;
+    promptWindow.classList.add("invisible");
+    background.classList.add("invisible");
+  }
+});
+
+//Bouton confirmer du prompt
+const confirmButton = document.querySelector(".confirm");
+
+//Appuyer sur le bouton confirme le changement de nom et ferme le prompt
+confirmButton.addEventListener("click", function () {
+  if (bakeryName.value !== "") {
+    bakery.innerHTML = bakeryName.value;
+    promptWindow.classList.add("invisible");
+    background.classList.add("invisible");
+  }
+});
+
+//Bouton annuler du prompt
+const cancelButton = document.querySelector(".cancel");
+
+//Appuyer sur le bouton ferme le prompt
+cancelButton.addEventListener("click", function () {
+  promptWindow.classList.add("invisible");
+  background.classList.add("invisible");
+});
+
+//Filtre qui floute l'arrière-plan du prompt
+const background = document.querySelector(".background");
+background.classList.add("invisible");
+
+//Quand on clique sur l'arrière-plan on ferme le prompt
+background.addEventListener("click", function () {
+  promptWindow.classList.add("invisible");
+  background.classList.add("invisible");
+});
+
+//---------------------------------------------------------------------------//
 
 const compteur = document.querySelector("#compteur");
 const clic = document.querySelector("#clic");
